e02f32f143c6beae6e7a67d6e6a640971fc8ac48
Enes Çoğalmış <enscglms@gmail.com>
2024-07-10 04:27:07 +0300

    order list resource with conditions

--- app/Http/Resources/OrderListResource.php
+++ app/Http/Resources/OrderListResource.php
@@ -22,9 +22,40 @@ class OrderListResource extends JsonResource
          */
 
         return [
-            'order' => [],
-            'customer' => [],
-            'products' => []
+            'order' => [
+                'id' => $this->id,
+                'order_no' => $this->order_no,
+                'order_date' => $this->order_date,
+                'status' => $this->whenLoaded('status', function () {
+                    return $this->status->status;
+                })
+            ],
+            'customer' => $this->whenLoaded('customer', function () {
+                return [
+                    'name' => $this->customer->name,
+                    'surname' => $this->customer->surname,
+                ];
+            }),
+            'products' => $this->whenLoaded('products', function () {
+                // Bir ürün siparişten sonra ürün tablosunda pasife alınmış olsa dahi bu endpointte listelenmelidir.
+                // Burada tam olarak ne istendiğini anlayamadım? Ürün tablosunda is_active gibi bir sütun mevcut değil.
+                // stock_status sütunundan bahsediliyorsa değeri 0 olsa dahi kayıtlarda gelecektir.
+                // Eğer "silinmiş" bir kayıttan bahsediliyorsada ->withTrashed() ile ayarlanmalı.
+                /*
+                $this->products->load(['product' => function ($query) {
+                    $query->withTrashed();
+                }])
+                */
+
+                $data = [];
+                $this->products->map(function ($product) use (&$data) {
+                    $data[] = [
+                        'id' => $product->product->id,
+                        'name' => $product->product->name,
+                    ];
+                });
+                return $data;
+            }),
         ];
     }
 }
